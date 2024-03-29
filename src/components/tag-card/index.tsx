20d1d30bd4a6ec1747ef37bd653e7a6e13d19337
KornelJanczak <korneljanczak10@gmail.com>
2024-03-29 16:25:08 +0100

    build: add useEffect and setTagList fc

--- src/components/tag-card/index.tsx
+++ src/components/tag-card/index.tsx
@@ -1,8 +1,10 @@
-import { useTags } from "@/common/tags/use-tags";
-import FilterBar from "./filter-bar";
 import { Card, CardContent, CardHeader } from "../ui/card";
-import NoResult from "../no-result";
 import Tags, { TagsSkeleton } from "./tags";
+import FilterBar from "./filter-bar";
+import NoResult from "../no-result";
+import { useTags } from "@/common/tags/use-tags";
+import { useTagsList } from "@/hooks/use-tags-list/use-tags-list";
+import { useEffect } from "react";
 
 export interface TagListProps {
   name: string;
@@ -10,25 +12,31 @@ export interface TagListProps {
 }
 
 export default function TagCard() {
-  // const { tagsList, setTagsList } = useTagsList();
-  const { tags, status } = useTags();
+  const { setTagsList, tagsList, numberOfTags } = useTagsList();
+  const { data, tags, status } = useTags();
 
   const isSuccess = status === "success";
-
   const isPending = status === "pending";
-
   const isError = status === "error";
 
+  useEffect(() => {
+    if (tags) {
+      setTagsList(tags.items);
+    }
+  }, [data, setTagsList, tags]);
+
+  const slicedTagList = tagsList.slice(0, numberOfTags);
+
   return (
-    <div className="m-4">
-      <Card className="container max-w-96 border-0">
-        <CardHeader className="px-2">
+    <div className="mx-6 my-10 md:my-24">
+      <Card className="container max-w-96 border-0 md:max-w-xl">
+        <CardHeader className="px-0">
           <FilterBar />
         </CardHeader>
-        <CardContent className="px-2">
-          {!isSuccess && <Tags tags={tags?.items || []} />}
+        <CardContent className="px-0">
+          {isSuccess && <Tags tags={slicedTagList} />}
           {isPending && <TagsSkeleton length={14} />}
-          {!isError && (
+          {isError && (
             <NoResult
               title="Error!"
               description="Failed to retrieve tags. Please try again later or contact support for assistance."
