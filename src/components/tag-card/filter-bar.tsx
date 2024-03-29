9309ae85afbda4ec1dfcc61a54d6050d78f8a895
KornelJanczak <korneljanczak10@gmail.com>
2024-03-29 15:05:29 +0100

    build: add handlers to component

--- src/components/tag-card/filter-bar.tsx
+++ src/components/tag-card/filter-bar.tsx
@@ -6,6 +6,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useTagsList } from "@/hooks/use-tags-list";
 
 const selectOptions = [
   { label: "Most related posts", value: "most-posts" },
@@ -15,18 +16,29 @@ const selectOptions = [
 ];
 
 export default function FilterBar() {
+  const { setNumberOfTags, setSortOption } = useTagsList();
+
+  const handleNumberOfTags = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNumberOfTags(Number(e.target.value));
+  };
+
+  const handleSortOption = (selectValue: string) => {
+    setSortOption(selectValue);
+  };
+
   return (
     <div className="flex justify-between items-center gap-x-5 w-full">
       <Input
+        onChange={handleNumberOfTags}
         placeholder="Number of tags"
         type="number"
         className="text-text focus:border-primary"
       />
-      <Select>
+      <Select onValueChange={handleSortOption}>
         <SelectTrigger className="focus:border-primary">
           <SelectValue placeholder="Sort by" />
         </SelectTrigger>
-        <SelectContent className="border-0">
+        <SelectContent onChange={(e) => console.log(e)} className="border-0">
           {selectOptions.map(({ label, value }) => (
             <SelectItem key={value} value={value} className="hover:bg-primary">
               {label}
